a2ad07ddc6b96bf7785680ed7e86822f2c42b5ce
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2017-10-09 11:18:19 +0200

    Dos receptores

---
+++ Practica1/Sesion4/Software/src/receptorArdu.cpp
@@ -0,0 +1,76 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Juan Manuel Fajardo Sarmiento
+// Francisco Javier Caracuel Beltrán
+//
+// TIC - 2017/2018 - 4º - GII - CCIA
+//
+// receptorArdu.cpp
+//
+// Programa para Arduino que recibe la señal del laser
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// Velocidad (en baudios) de las comunicaciones serie
+#define UART_BAUD_RATE 9600
+
+#include <ticcomardu.h>
+#include <avr/interrupt.h>
+#include <uart.h>
+
+// Programa principal
+int main(void){
+    
+    // Dato a leer desde Arduino
+    unsigned char dato;
+    
+    char cadena[101];
+    unsigned int n=0;
+    
+    // Inicialización del puerto UART con la velocidad en baudios del puerto
+    // y la velocidad del procesador
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE), F_CPU);
+    
+    // Activación de las interrupciones hardware para control del puerto serie
+    sei();
+    
+    // Inicialización del fotorreceptor
+    initLaserReceiver();
+    
+    // Bucle infinito
+    while(1){
+        
+        n = 0;
+        
+        // Se rellena el buffer
+        for(int i=0; i<1000; i++){
+            
+            // Se recibe el dato
+            recvLaserBit(dato);
+            
+            // Si es ráfaga corta, incluimos en el buffer un 0
+            if(dato == LASER_DOT)
+                cadena[n] = '0';
+            
+            // Si es ráfaga larga, incluimos en el buffer un 1
+            else if(dato == LASER_DASH)
+                cadena[n] = '1';
+            
+            // Si no se recibe nada, se incluye en el buffer un ?
+            else
+                cadena[n] = '?';
+            
+            // Se pasa al siguiente carácter en cadena
+            n++;
+            
+        }
+        
+        // Se envía el mensaje al PC
+        cadena[n] = '\0';
+        arduSendUSB(cadena);
+        
+    }
+    
+    return 0;
+    
+}
---
+++ Practica1/Sesion4/Software/src/receptorPC.cpp
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Juan Manuel Fajardo Sarmiento
+// Francisco Javier Caracuel Beltrán
+//
+// TIC - 2017/2018 - 4º - GII - CCIA
+//
+// receptorPC.cpp
+//
+// Programa receptor para el PC. Mostrará los datos que llegan del fotorreceptor
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cstring>
+#include <ticcommpc.h>
+#include <iostream>
+#include <unistd.h>
+#include <termios.h>
+
+using namespace std;
+
+// Puerto de comunicaciones con arduino
+#define USBPORT "/dev/ttyACM0"
+
+// Programa principal
+int main(int argc, char *argv[]){
+    
+    // Descriptor de fichero del puerto USB
+    int fd;
+    
+    // Buffer de salida. Tamaño máximo de 128 caracteres.
+    char buf[129];
+    
+    // Se inicializa el puerto
+    fd = InicializarUSB(USBPORT);
+    
+    if(fd==-1){
+        cout << "Error inicializando el puerto " << USBPORT << endl;
+        return 0;
+    }
+    
+    // Bucle principal
+    while(1){
+        
+        // Se leen datos del USB
+        if(receiveUSB(fd, buf)){
+            
+            cout << "Mensaje recibido: " << buf << endl;
+            
+        } else{
+            
+            cout << "\tError recibiendo datos\n\n";
+            CerrarUSB(fd);
+            return 0;
+            
+        }
+        
+    }
+    
+    // Se cierra el puerto USB
+    CerrarUSB(fd);
+    return 0;
+    
+}
