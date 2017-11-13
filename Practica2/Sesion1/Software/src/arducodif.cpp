c50e0a045b240a482960b2afbc9693c23fdaae6f
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2017-11-13 09:50:57 +0100

    Base de la práctica 2

---
+++ Practica2/Sesion1/Software/src/arducodif.cpp
@@ -0,0 +1,280 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Juan Manuel Fajardo Sarmiento
+// Francisco Javier Caracuel Beltrán
+//
+// TIC - 2017/2018 - 4º - GII - CCIA
+//
+// arducodif.cpp
+//
+// Biblioteca para codificación de código Morse
+//
+////////////////////////////////////////////////////////////////////////////////
+#include <ctype.h>
+#include <stdlib.h>
+void codificaSimboloMorse(const char corig, char &ccodif, unsigned char &nUtil){
+
+	switch(toupper(corig)){
+
+		case 'A':
+			ccodif = 0b00000001;
+			nUtil = 2;
+			break;
+		case 'B':
+			ccodif = 0b00001000;
+			nUtil = 4;
+			break;
+		case 'C':
+			ccodif = 0b00001010;
+			nUtil = 4;
+			break;
+		case 'D':
+			ccodif = 0b00000100;
+			nUtil = 3;
+			break;
+		case 'E':
+			ccodif = 0b00000000;
+			nUtil = 1;
+			break;
+		case 'F':
+			ccodif = 0b00000010;
+			nUtil = 4;
+			break;
+		case 'G':
+			ccodif = 0b00000110;
+			nUtil = 3;
+			break;
+		case 'H':
+			ccodif = 0b00000000;
+			nUtil = 4;
+			break;
+		case 'I':
+			ccodif = 0b00000000;
+			nUtil = 2;
+			break;
+		case 'J':
+			ccodif = 0b00000111;
+			nUtil = 4;
+			break;
+		case 'K':
+			ccodif = 0b00000101;
+			nUtil = 3;
+			break;
+		case 'L':
+			ccodif = 0b00000100;
+			nUtil = 4;
+			break;
+		case 'M':
+			ccodif = 0b00000011;
+			nUtil = 2;
+			break;
+		case 'N':
+			ccodif = 0b00000010;
+			nUtil = 2;
+			break;
+		case 'O':
+			ccodif = 0b00000111;
+			nUtil = 3;
+			break;
+		case 'P':
+			ccodif = 0b00000110;
+			nUtil = 4;
+			break;
+		case 'Q':
+			ccodif = 0b00001101;
+			nUtil = 4;
+			break;
+		case 'R':
+			ccodif = 0b00000010;
+			nUtil = 3;
+			break;
+		case 'S':
+			ccodif = 0b00000000;
+			nUtil = 3;
+			break;
+		case 'T':
+			ccodif = 0b00000001;
+			nUtil = 1;
+			break;
+		case 'U':
+			ccodif = 0b00000001;
+			nUtil = 3;
+			break;
+		case 'V':
+			ccodif = 0b00000001;
+			nUtil = 4;
+			break;
+		case 'W':
+			ccodif = 0b00000011;
+			nUtil = 3;
+			break;
+		case 'X':
+			ccodif = 0b00001001;
+			nUtil = 4;
+			break;
+		case 'Y':
+			ccodif = 0b00001011;
+			nUtil = 4;
+			break;
+		case 'Z':
+			ccodif = 0b00001100;
+			nUtil = 4;
+			break;
+		case '.':
+			ccodif = 0b00010101;
+			nUtil = 6;
+			break;
+		case ',':
+			ccodif = 0b00101011;
+			nUtil = 6;
+			break;
+		case ';':
+			ccodif = 0b00001100;
+			nUtil = 6;
+			break;
+		case ' ':
+			ccodif = 0b00010010;
+			nUtil = 6;
+			break;
+
+	}
+
+}
+
+void decodificaSimboloMorse(const char ccodif, const unsigned char nUtils, char &decodif){
+
+	switch(nUtils){
+		case 1:
+			switch(ccodif){
+				case 0b00000000:
+					decodif = 'E';
+					break;
+				case 0b00000001:
+					decodif = 'T';
+					break;
+			}
+			break;
+
+		case 2:
+			switch(ccodif){
+				case 0b00000001:
+					decodif = 'A';
+					break;
+				case 0b00000000:
+					decodif = 'I';
+					break;
+				case 0b00000011:
+					decodif = 'M';
+					break;
+				case 0b00000010:
+					decodif = 'N';
+					break;
+			}
+			break;
+
+		case 3:
+			switch(ccodif){
+				case 0b00000100:
+					decodif = 'D';
+					break;
+				case 0b00000110:
+					decodif = 'G';
+					break;
+				case 0b00000101:
+					decodif = 'K';
+					break;
+				case 0b00000111:
+					decodif = 'O';
+					break;
+				case 0b00000010:
+					decodif = 'R';
+					break;
+				case 0b00000000:
+					decodif = 'S';
+					break;
+				case 0b00000001:
+					decodif = 'U';
+					break;
+				case 0b00000011:
+					decodif = 'W';
+					break;
+			}
+			break;
+
+		case 4:
+			switch(ccodif){
+				case 0b00001000:
+					decodif = 'B';
+					break;
+				case 0b00001010:
+					decodif = 'C';
+					break;
+				case 0b00000010:
+					decodif = 'F';
+					break;
+				case 0b00000000:
+					decodif = 'H';
+					break;
+				case 0b00000111:
+					decodif = 'J';
+					break;
+				case 0b00000100:
+					decodif = 'L';
+					break;
+				case 0b00000110:
+					decodif = 'P';
+					break;
+				case 0b00001101:
+					decodif = 'Q';
+					break;
+				case 0b00000001:
+					decodif = 'V';
+					break;
+				case 0b00001001:
+					decodif = 'X';
+					break;
+				case 0b00001011:
+					decodif = 'Y';
+					break;
+				case 0b00001100:
+					decodif = 'Z';
+					break;
+			}
+			break;
+
+		case 6:
+		switch(ccodif){
+				case 0b00010101:
+					decodif = '.';
+					break;
+				case 0b00101011:
+					decodif = ',';
+					break;
+				case 0b00001100:
+					decodif = ';';
+					break;
+				case 0b00010010:
+					decodif = ' ';
+					break;
+			}
+			break;
+
+	}
+
+}
+
+void codificador(const char *orig, const int nOrig, char *codif, unsigned char *util){
+
+	for (int i=0; i<nOrig; i++){
+		codificaSimboloMorse(orig[i], codif[i], util[i]);
+	}
+
+}
+
+void decodificador(const char *codif, const unsigned char *utiles,  const  int nCodif, char *decodif){
+
+	for (int i=0; i<nCodif; i++){
+		decodificaSimboloMorse(codif[i], utiles[i], decodif[i]);
+	}
+
+}
---
+++ Practica2/Sesion1/Software/src/emisorPC.cpp
@@ -0,0 +1,150 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Juan Manuel Fajardo Sarmiento
+// Francisco Javier Caracuel Beltrán
+//
+// TIC - 2017/2018 - 4º - GII - CCIA
+//
+// emirsorPC.cpp
+//
+// Programa para PC que envía datos a Arduino
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cstring>
+#include <ticcommpc.h>
+#include <iostream>
+#include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
+#include <ctype.h>
+
+using namespace std;
+
+// Puerto de comunicaciones con arduino
+
+/* MODIFICAR EL PUERTO CON AQUEL DONDE SE VAYA A CONECTAR ARDUINO */
+#define USBPORT0 "/dev/ttyACM0"
+#define USBPORT1 "/dev/ttyACM1"
+//#define USBPORT0 "/dev/ttyUSB0"
+//#define USBPORT1 "/dev/ttyUSB1"
+
+int main(int argc, char *argv[]) {
+
+    int port = 0;
+
+    // Se lee el argumento recibido para cambiar el puerto del Arduino
+    if(argc == 2 && atoi(argv[1]) == 1){
+      port = 1;
+    }
+
+    // Descriptor de puerto del puerto USB
+    int fd;
+
+    // Buffer de salida. Tamaño máximo de 100 caracteres
+    char buf[100];
+
+    // Número de datos en el buffer
+    int ndata;
+
+    int aux;
+
+    // Inicializamos puerto
+    if(port == 0){
+      fd = InicializarUSB(USBPORT0);
+    } else{
+      fd = InicializarUSB(USBPORT1);
+    }
+
+    if (fd == -1) {
+
+        if(port == 0){
+            cout << "Error inicializando puerto " << USBPORT0 << endl;
+        } else{
+            cout << "Error inicializando puerto " << USBPORT1 << endl;
+        }
+
+        return 0;
+
+    }
+
+    // Bucle hasta que salgamos (cuando no introduzcamos nada)
+    do{
+
+        cout << "Escriba algo para enviar a Arduino: ";
+        cin.getline(buf, 100);
+
+        // Tamaño del buffer de salida
+        ndata = strlen(buf);
+
+        // Se comprueba si los carácteres recibidos son correctos
+        int i = 0;
+
+        while(i<ndata){
+
+            // Si el carácter está en minúscula, se convierte a mayúscula
+            if(buf[i] >= 'a' && buf[i] <= 'z'){
+                buf[i] = toupper(buf[i]);
+            }
+
+            // Si es algún carácter inválido se termina el programa
+            if(!((buf[i] >= 'A' && buf[i] <= 'Z') || buf[i] == '.' ||
+                buf[i] == ',' || buf[i] == ';'  || buf[i] == ' ' )){
+
+                cout << "Error al recibir caracteres inválidos" << endl;
+                ndata = 0;
+                aux = 0;
+
+            }
+
+            i++;
+
+        }
+
+        if (ndata > 0) {
+
+            aux = write(fd, buf, ndata);
+
+            tcflush(fd, TCIFLUSH);
+
+            cout << "\nBytes enviados " << aux << "/" << ndata << endl;
+
+            // Comprobamos errores en el envío
+            if (aux < ndata) {
+
+                cout << "Error, en el buffer tiene " << ndata << " bytes pero se enviaron " << aux << endl;
+                ndata = 0;
+
+            }
+
+            // Si se envió algun dato, esperamos respuesta de Arduino
+            if (aux > 0) {
+
+                do{
+
+                    aux = receiveUSB(fd, buf);
+
+                    //cout << "\nMensaje: " << buf << endl;
+
+                }while(buf == "OK");
+
+                if (aux > 0)
+                    cout << "\nMensaje: " << buf << endl;
+                else
+                    cout << "ERROR RECIBIENDO DATOS\n";
+
+            }
+
+        }
+
+    // Se sale si no enviamos ningún caracter
+    } while (ndata > 0);
+
+    // Cerramos el puerto USB
+    CerrarUSB(fd);
+
+    cout << "\nFin del programa.\n\n";
+
+    return 0;
+
+}
